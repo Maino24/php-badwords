1cec537ec45782279e39bcf4d30025527fa0c13b
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-12 21:14:54 +0100

    sostituite parole inserite in input

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+
+
+
+$parolaCensurata = $_GET["parolaCensurata"];
+//echo $parolaCensurata;
+
+$censura = '***';
+
+$parola = str_replace($parolaCensurata,$censura,$paragrafo);
+
+//$array_stringa = explode('', $paragrafo);
+
+//var_dump($array_stringa);
+
 $paragrafo = 'Vedi che sdegna li argomenti umani,
 sì che remo non vuol, né altro velo
 che l ali sue, tra liti sì lontani
@@ -12,9 +26,6 @@ che la dolcezza ancor dentro mi suona';
 
 /* LUNGHEZZA PARAGRAFO */
 $lunghezzaParagrafo = strlen($paragrafo);
-
-$parolaCensurata = $_GET["parolaCensurata"];
-echo $parolaCensurata;
 
 ?>
 
@@ -31,6 +42,18 @@ echo $parolaCensurata;
 
 <p><?php echo $paragrafo ?></p>
 <p><?php echo $lunghezzaParagrafo ?></p>
+
+
+
+<form action="" method="GET">
+
+    <input type="text" name="parolaCensurata">
+    <button type="">Invio</button>
+
+</form>
+
+<?php $parola = str_replace($parolaCensurata,$censura,$paragrafo);?>
+<p><?php echo $parola ?></p>
 
 </body>
 </html>
