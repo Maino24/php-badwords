4a4e67eba552160efcaea3cf78508bc43570cb3a
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-12 16:33:14 +0100

    stampato paragrafo

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$paragrafo = 'Vedi che sdegna li argomenti umani,
+sì che remo non vuol, né altro velo
+che l ali sue, tra liti sì lontani
+Ohi ombre vane, fuor che ne laspetto!
+tre volte dietro a lei le mani avvinsi,
+e tante mi tornai con esse al petto
+Amor che ne la mente mi ragiona
+cominciò elli allor sì dolcemente,
+che la dolcezza ancor dentro mi suona'
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<p><?php echo $paragrafo ?></p>
+
+</body>
+</html>
